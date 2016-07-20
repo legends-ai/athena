8b89b2d3ad0203af2886c3fb54f859028f599bda
Ian Macalinao <me@ian.pw>
2016-07-19 22:03:13 -0700

    update plane script

--- build.sbt
+++ build.sbt
@@ -3,7 +3,7 @@ version := "0.0.1"
 scalaVersion := "2.10.5"
 
 libraryDependencies ++= Seq(
- "org.apache.spark" %% "spark-core" % "1.2.0" % "provided",
+ "org.apache.spark" %% "spark-core" % "1.6.2" % "provided",
  "com.datastax.spark" %% "spark-cassandra-connector" % "1.2.0"
 )
 
--- script/plane.sh
+++ script/plane.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+DIR=`dirname $0`
+
 # Script to run stuff from the plane
 spark-submit \
     --class ai.legends.athena.Main \
-    --master local \
-    ../target/scala-2.10/legendsai-athena-assembly-0.0.1.jar
+    --master local[4] \
+    $DIR/../target/scala-2.10/legendsai-athena-assembly-0.0.1.jar
