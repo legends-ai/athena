cd32304487a4439b51e0435ec2675af759e90003
Ian Macalinao <me@ian.pw>
2016-07-19 19:48:04 -0700

    init

---
+++ build.sbt
@@ -0,0 +1,8 @@
+name := "legendsai-athena"
+version := "0.0.1"
+scalaVersion := "2.10.5"
+
+libraryDependencies ++= Seq(
+ "org.apache.spark" %% "spark-core" % "1.2.0" % "provided",
+ "com.datastax.spark" %% "spark-cassandra-connector" % "1.2.0"
+)
---
+++ src/main/scala/ai/legends/athena/Main.scala
@@ -0,0 +1,13 @@
+package ai.legends.athena
+
+import org.apache.spark.{ SparkConf, SparkContext }
+import com.datastax.spark.connector._
+
+object Main {
+  def main(args: Array[String]) {
+    val conf = new SparkConf(true).set("spark.cassandra.connection.host", "127.0.0.1")
+    val sc = new SparkContext(conf)
+    val rdd = sc.cassandraTable("athena", "matches")
+    println(rdd.first)
+  }
+}
