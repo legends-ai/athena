0da8c8e3de539131724e5fb5fc52cfcc731bbdaa
Ian Macalinao <me@ian.pw>
2016-07-26 01:36:57 -0400

    Move fixtures to separate file

---
+++ src/test/scala/ai/legends/athena/matches/MatchFixtures.scala
@@ -0,0 +1,145 @@
+package ai.legends.athena.matches
+
+import org.json4s._
+import org.json4s.jackson.JsonMethods._
+
+object MatchFixtures {
+
+  val statsStr =
+    """
+    {
+      "assists": 2,
+      "champLevel": 11,
+      "combatPlayerScore": 0,
+      "deaths": 4,
+      "doubleKills": 0,
+      "firstBloodAssist": false,
+      "firstBloodKill": true,
+      "firstInhibitorAssist": false,
+      "firstInhibitorKill": false,
+      "firstTowerAssist": false,
+      "firstTowerKill": false,
+      "goldEarned": 7992,
+      "goldSpent": 7225,
+      "inhibitorKills": 0,
+      "item0": 1409,
+      "item1": 2033,
+      "item2": 3009,
+      "item3": 3742,
+      "item4": 1029,
+      "item5": 0,
+      "item6": 3340,
+      "killingSprees": 1,
+      "kills": 6,
+      "largestCriticalStrike": 0,
+      "largestKillingSpree": 5,
+      "largestMultiKill": 1,
+      "magicDamageDealt": 45218,
+      "magicDamageDealtToChampions": 7375,
+      "magicDamageTaken": 3572,
+      "minionsKilled": 11,
+      "neutralMinionsKilled": 62,
+      "neutralMinionsKilledEnemyJungle": 7,
+      "neutralMinionsKilledTeamJungle": 55,
+      "objectivePlayerScore": 0,
+      "pentaKills": 0,
+      "physicalDamageDealt": 17040,
+      "physicalDamageDealtToChampions": 2002,
+      "physicalDamageTaken": 17096,
+      "quadraKills": 0,
+      "sightWardsBoughtInGame": 0,
+      "totalDamageDealt": 72640,
+      "totalDamageDealtToChampions": 9944,
+      "totalDamageTaken": 23109,
+      "totalHeal": 6020,
+      "totalPlayerScore": 0,
+      "totalScoreRank": 0,
+      "totalTimeCrowdControlDealt": 578,
+      "totalUnitsHealed": 1,
+      "towerKills": 0,
+      "tripleKills": 0,
+      "trueDamageDealt": 10381,
+      "trueDamageDealtToChampions": 566,
+      "trueDamageTaken": 2440,
+      "unrealKills": 0,
+      "visionWardsBoughtInGame": 0,
+      "wardsKilled": 4,
+      "wardsPlaced": 11,
+      "winner": false
+    }
+    """
+
+  val statsJSON = parse(MatchFixtures.statsStr).asInstanceOf[JObject]
+
+  val stats = ParticipantStats(
+    winner = false,
+    champLevel = 11,
+    combatPlayerScore = 0,
+    goldEarned = 7992,
+    goldSpent = 7225,
+    objectivePlayerScore = 0,
+    totalHeal = 6020,
+    totalPlayerScore = 0,
+    totalScoreRank = 0,
+    totalTimeCrowdControlDealt = 578,
+    totalUnitsHealed = 1,
+    damage = ParticipantDamage(
+      magicDamageDealt = 45218,
+      magicDamageDealtToChampions = 7375,
+      magicDamageTaken = 3572,
+      physicalDamageDealt = 17040,
+      physicalDamageDealtToChampions = 2002,
+      physicalDamageTaken = 17096,
+      totalDamageDealt = 72640,
+      totalDamageDealtToChampions = 9944,
+      trueDamageDealt = 10381,
+      trueDamageDealtToChampions = 566,
+      trueDamageTaken = 2440,
+      largestCriticalStrike = 0,
+      totalDamageTaken = 23109
+    ),
+    firsts = ParticipantFirsts(
+      firstBloodAssist = false,
+      firstBloodKill = true,
+      firstInhibitorAssist = false,
+      firstInhibitorKill = false,
+      firstTowerAssist = false,
+      firstTowerKill = false
+    ),
+    items = ParticipantItems(
+      item0 = 1409,
+      item1 = 2033,
+      item2 = 3009,
+      item3 = 3742,
+      item4 = 1029,
+      item5 = 0,
+      item6 = 3340
+    ),
+    kda = ParticipantKDA(
+      assists = 2,
+      deaths = 4,
+      kills = 6,
+      killingSprees = 1,
+      pentaKills = 0,
+      quadraKills = 0,
+      tripleKills = 0,
+      unrealKills = 0,
+      largestKillingSpree = 5,
+      largestMultiKill = 1,
+      inhibitorKills = 0,
+      minionsKilled = 11,
+      towerKills = 0,
+      neutralMinionsKilled = 62,
+      neutralMinionsKilledEnemyJungle = 7,
+      neutralMinionsKilledTeamJungle = 55,
+      doubleKills = 0
+    ),
+    wards = ParticipantWards(
+      visionWardsBoughtInGame = 0,
+      sightWardsBoughtInGame = 0,
+      wardsKilled = 4,
+      wardsPlaced = 11
+    )
+  )
+
+}
