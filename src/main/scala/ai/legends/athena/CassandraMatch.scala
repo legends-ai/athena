f5eb16327f9ac9fbace2bcb61e97bd76e1d1a2a7
Ian Macalinao <me@ian.pw>
2016-07-26 03:02:19 -0400

    Move match parsing logic into Match.scala

--- src/main/scala/ai/legends/athena/CassandraMatch.scala
+++ src/main/scala/ai/legends/athena/CassandraMatch.scala
@@ -2,9 +2,6 @@ package ai.legends.athena
 
 import ai.legends.athena.matches.Match
 import ai.legends.athena.matches.Participant
-import org.json4s._
-import org.json4s.JsonDSL._
-import org.json4s.jackson.JsonMethods._
 
 case class CassandraMatch(
   id: String,
@@ -16,25 +13,7 @@ case class CassandraMatch(
 
   /** toMatch converts the Cassandra match to JSON. */
   def toMatch(): Match = {
-    implicit val formats = DefaultFormats
-    val m = parse(this.body)
-    val transformed = m.asInstanceOf[JObject] ~ ("participants" -> transformParticipants(m \ "participants"))
-    println("+\n" * 10)
-    println(((m \ "participants")(0) \ "stats") \ "winner")
-    println(((transformed \ "participants")(0) \ "stats") \ "winner")
-    println(((transformed \ "participants")(1) \ "stats") \ "winner")
-    println("+\n" * 10)
-    transformed.extract[Match]
-  }
-
-  def transformParticipants(json: JValue): JArray = {
-    json.asInstanceOf[JArray].transform {
-      case obj: JObject => Participant.transform(obj)
-    }.asInstanceOf[JArray]
-    // json.asInstanceOf[JArray].arr.map {
-    //   case obj: JObject => Participant.transform(obj)
-    //   case x => x
-    // }
+    Match.fromJSON(this.body)
   }
 
 }
--- src/main/scala/ai/legends/athena/matches/Match.scala
+++ src/main/scala/ai/legends/athena/matches/Match.scala
@@ -1,7 +1,8 @@
 package ai.legends.athena.matches
 
-import org.json4s.JsonAST.JValue
+import org.json4s._
 import org.json4s.JsonDSL._
+import org.json4s.jackson.JsonMethods._
 
 case class MatchTimeline (
   frameInterval: Int,
@@ -67,3 +68,32 @@ case class Match (
   queueType: String,
   matchId: Int
 )
+
+object Match {
+
+  def fromJSON(json: String): Match = {
+    val m = parse(json)
+    implicit val formats = DefaultFormats
+
+    val transformed = m.asInstanceOf[JObject] ~ ("participants" -> transformParticipants(m \ "participants"))
+    println("+\n" * 10)
+    println(((m \ "participants")(0) \ "stats") \ "winner")
+    println(((transformed \ "participants")(0) \ "stats") \ "winner")
+    println(((transformed \ "participants")(1) \ "stats") \ "winner")
+    println("+\n" * 10)
+    transformed.extract[Match]
+ 
+  }
+
+
+  def transformParticipants(json: JValue): JArray = {
+    json.asInstanceOf[JArray].transform {
+      case obj: JObject => Participant.transform(obj)
+    }.asInstanceOf[JArray]
+    // json.asInstanceOf[JArray].arr.map {
+    //   case obj: JObject => Participant.transform(obj)
+    //   case x => x
+    // }
+  }
+
+}
