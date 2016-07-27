a9f14e2be27cf13155b83ace0ed89091e608629c
Ian Macalinao <me@ian.pw>
2016-07-27 05:03:03 -0400

    Use default values in case class constructor

--- src/test/scala/ai/legends/athena/champions/ChampionTotalsSpec.scala
+++ src/test/scala/ai/legends/athena/champions/ChampionTotalsSpec.scala
@@ -6,29 +6,29 @@ class ChampionTotalsSpec extends FlatSpec with Matchers {
 
   it should "merge plays properly" in {
     val total1 = ChampionTotals(
-      25,
-      Map(1 -> 3, 2 -> 4),
-      4,
-      1,
-      2,
-      3
+      plays =  25,
+      playsByPlayer = Map(1 -> 3, 2 -> 4),
+      wins = 4,
+      kills = 1,
+      deaths = 2,
+      assists = 3
     )
     val total2 = ChampionTotals(
-      13,
-      Map(1 -> 2, 3 -> 5),
-      2,
-      4,
-      5,
-      6
+      plays = 13,
+      playsByPlayer = Map(1 -> 2, 3 -> 5),
+      wins = 2,
+      kills = 4,
+      deaths = 5,
+      assists = 6
     )
     val result = total1 + total2
     result should be(ChampionTotals(
-      38,
-      Map(1 -> 5, 2 -> 4, 3 -> 5),
-      6,
-      5,
-      7,
-      9
+      plays = 38,
+      playsByPlayer = Map(1 -> 5, 2 -> 4, 3 -> 5),
+      wins = 6,
+      kills = 5,
+      deaths = 7,
+      assists = 9
     ))
   }
 
