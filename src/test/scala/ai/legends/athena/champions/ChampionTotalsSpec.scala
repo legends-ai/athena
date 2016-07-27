fb8cc0c662601f9e7d16207606c6bd1670560cc6
Ian Macalinao <me@ian.pw>
2016-07-27 03:42:08 -0400

    Fix broken test

--- src/test/scala/ai/legends/athena/champions/ChampionTotalsSpec.scala
+++ src/test/scala/ai/legends/athena/champions/ChampionTotalsSpec.scala
@@ -7,16 +7,19 @@ class MatchSpec extends FlatSpec with Matchers {
   it should "merge plays properly" in {
     val total1 = ChampionTotals(
       25,
-      Map(1 -> 3, 2 -> 4)
+      Map(1 -> 3, 2 -> 4),
+      4
     )
     val total2 = ChampionTotals(
       13,
-      Map(1 -> 2, 3 -> 5)
+      Map(1 -> 2, 3 -> 5),
+      2
     )
     val result = total1 + total2
     result should be(ChampionTotals(
       38,
-      Map(1 -> 5, 2 -> 4, 3 -> 5)
+      Map(1 -> 5, 2 -> 4, 3 -> 5),
+      6
     ))
   }
 
