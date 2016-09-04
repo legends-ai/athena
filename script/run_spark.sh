16f82b967a58bbce25575c59cebebfd00e464919
Ian Macalinao <me@ian.pw>
2016-09-04 10:28:18 -0500

    update run script

--- script/run_spark.sh
+++ script/run_spark.sh
@@ -5,4 +5,4 @@ DIR=`dirname $0`
 spark-submit \
     --class ai.legends.athena.Main \
     --master local[4] \
-    $DIR/../target/scala-2.10/legendsai-athena-assembly-0.0.1.jar
+    $DIR/../target/scala-2.11/legendsai-athena-assembly-0.0.1.jar
