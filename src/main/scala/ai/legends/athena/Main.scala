265b6c577415e849948c1382a540375e9672151d
Ian Macalinao <me@ian.pw>
2016-09-04 15:45:24 -0500

    Match sums save

--- src/main/scala/ai/legends/athena/Main.scala
+++ src/main/scala/ai/legends/athena/Main.scala
@@ -7,11 +7,6 @@ import ai.legends.athena.cassandra.CassandraMatchSum
 import ai.legends.athena.sum.MatchSumRow
 import ai.legends.athena.sum.Permuter
 
-import org.json4s._
-import org.json4s.jackson.JsonMethods._
-import org.json4s.jackson.Serialization
-import org.json4s.jackson.Serialization.{read, write}
-
 object Main {
   def main(args: Array[String]) {
     val conf = new SparkConf(true).set("spark.cassandra.connection.host", "127.0.0.1")
@@ -26,10 +21,8 @@ object Main {
 
     // Convert to Cassandra equivalent
     val matchSums = permutations.map(matchSumRow => CassandraMatchSum(matchSumRow))
-    // matchSums.saveToCassandra("athena", "match_sums", AllColumns)
+    matchSums.saveToCassandra("athena", "match_sums", AllColumns)
 
-    // implicit val formats = Serialization.formats(NoTypeHints)
-    // println(write(champs.head))
-    println(matchSums.count())
+    println("Wrote " + matchSums.count() + " match sums.")
   }
 }
